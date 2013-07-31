d5a8092d36d834661a7d748699aee5fbbe736c43
ariane_souza <ariane_souza@10.1.49.220>
2013-07-31 17:21:43 -0300

    pacote persistencia e classe funcionario, que receberá os dados de funcionario

---
+++ src/br/senai/sc/view/JanelaPrincipal.java
@@ -0,0 +1,34 @@
+
+package br.senai.sc.view;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Classe que controla toda a parte visual da aplicação
+ * @version  1.0 31/07/13
+ * @author ariane_souza
+ */
+public class JanelaPrincipal {
+    
+    public void mostraMenu (){
+        int opcao;
+        
+        do{
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe a opcao:\n1-Funcionário\n2-Cliente\n3-Fornecedor\n4-Transportadora\n5-Sair"));
+        
+        switch(opcao){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inexistente");
+            
+        }
+    }
+    
+}
