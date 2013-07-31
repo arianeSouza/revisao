e2674fa39c4af46649591094e538b57d66fdcfae
ariane_souza <ariane_souza@10.1.49.220>
2013-07-31 16:19:58 -0300

    Cria classe Cliente Pessoa Juridica

---
+++ src/br/senai/sc/model/negocio/ClientePJ.java
@@ -0,0 +1,67 @@
+
+package br.senai.sc.model.negocio;
+
+/**
+ *Classe que representa Pessoa Juridica
+ * @version 1.0 31/07/13
+ * @author ariane_souza
+ */
+public class ClientePJ extends PessoaJuridica {
+    
+    private double limite;
+    private int nrConta;
+    private char categoria;
+    private int qtVezesComprou;
+    private double vlTotalGasto;
+
+    public ClientePJ(double limite, int nrConta, char categoria, int qtVezesComprou, double vlTotalGasto, String cnpj, String razaoSocial, String nomeFantasia) {
+        super(cnpj, razaoSocial, nomeFantasia);
+        this.limite = limite;
+        this.nrConta = nrConta;
+        this.categoria = categoria;
+        this.qtVezesComprou = qtVezesComprou;
+        this.vlTotalGasto = vlTotalGasto;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public int getNrConta() {
+        return nrConta;
+    }
+
+    public void setNrConta(int nrConta) {
+        this.nrConta = nrConta;
+    }
+
+    public char getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(char categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQtVezesComprou() {
+        return qtVezesComprou;
+    }
+
+    public void setQtVezesComprou(int qtVezesComprou) {
+        this.qtVezesComprou = qtVezesComprou;
+    }
+
+    public double getVlTotalGasto() {
+        return vlTotalGasto;
+    }
+
+    public void setVlTotalGasto(double vlTotalGasto) {
+        this.vlTotalGasto = vlTotalGasto;
+    }
+    
+    
+}
