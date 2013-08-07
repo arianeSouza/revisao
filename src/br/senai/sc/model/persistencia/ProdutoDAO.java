2b4770fc0a8f28572ff63ca7384a18261884134b
ariane_souza <ariane_souza@10.1.48.222>
2013-08-07 15:58:46 -0300

    Cria classe ProdutoDAO

---
+++ src/br/senai/sc/model/persistencia/ProdutoDAO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.persistencia;
+
+/**
+ *
+ * @author ariane_souza
+ */
+public class ProdutoDAO {
+     
+}
